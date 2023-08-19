e93d492286f28466dfe5906b73b6d0b6e499948b
ivanczar <ivanczar2013@gmail.com>
2023-08-19 17:55:17 +1200

    fixed open mout eye reset

--- mouth.py
+++ mouth.py
@@ -37,11 +37,11 @@ class Mouth:
                     pop_index = len(english_arr) - 1
                     random_word.update_color_arr(pop_index, None)
                     english_arr.pop()
-                    mouth_total = 0
+                    self.total = 0
                 else:
                     self.total += 1
-                    left_total = 0
-                    right_total = 0
+                    self.eyes.left_total = 0
+                    self.eyes.right_total = 0
                     morse_to_english(morse_arr, english_arr)
                     morse_arr.clear()
             self.counter = 0
