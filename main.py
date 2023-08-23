a8655481a0befeb6ff385c19cf94da879d4009bc
ivanczar <ivanczar2013@gmail.com>
2023-08-23 15:01:12 +1200

    refactored

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-# TODO: instantiate classes
-# TODO: define configs
-
-"""
-TODO: QUESTIONS
-Should i instantiate all objects (eye,mouth,graphics-helper) in main and pass them into App class?
-    Should videostream, detector, and predictor be created in App or in main and passed into App?
-Difference between passing into class constructor and then using self. within class methods vs just passing object as argumentto method (i.e pass eyes object to detect_mouth instead of passing to constructor of Mouth)
-Should i structure classes as packages? or is this flat structure OK?
-Anything else i could improve on structure-wise?
-"""
 import argparse
 import configparser
 
@@ -23,7 +12,6 @@ args = vars(ap.parse_args())
 config_data = configparser.ConfigParser()
 config_data.read("config.ini")
 blink_config = config_data["BLINK"]
-cv2_config = config_data["CV2"]
 FRAME_WIDTH = 750
 
 app = App(blink_config, args, 750)
